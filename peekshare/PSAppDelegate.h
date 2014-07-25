85bdbe1c746084b0691fa49413824ad9896be427
Dennis Schmidt <dmrschmidt@gmail.com>
2014-07-25 20:01:07 +0200

    Initial Commit

---
+++ peekshare/PSAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  PSAppDelegate.h
+//  peekshare
+//
+//  Created by Dennis Schmidt on 25.07.14.
+//  Copyright (c) 2014 Dennis Schmidt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PSAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ peekshare/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  peekshare
+//
+//  Created by Dennis Schmidt on 25.07.14.
+//  Copyright (c) 2014 Dennis Schmidt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "PSAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+  @autoreleasepool {
+      return UIApplicationMain(argc, argv, nil, NSStringFromClass([PSAppDelegate class]));
+  }
+}
