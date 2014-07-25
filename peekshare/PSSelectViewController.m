cb66c49e828f62d55863d8a89f7d9726087eeab5
Dennis Schmidt <dmrschmidt@gmail.com>
2014-07-25 20:50:28 +0200

    initial selection

--- peekshare/PSSelectViewController.m
+++ peekshare/PSSelectViewController.m
@@ -7,12 +7,15 @@
 //
 
 #import "PSSelectViewController.h"
+#import "TSAssetsPickerController.h"
 
-@interface PSSelectViewController ()
-
+@interface PSSelectViewController () <UINavigationControllerDelegate, TSAssetsPickerControllerDelegate, TSAssetsPickerControllerDataSource>
+@property(nonatomic, strong) TSAssetsPickerController *assetsPickerController;
 @end
 
 @implementation PSSelectViewController
+
+#pragma mark - View Lifecycle
 
 - (void)viewDidLoad
 {
@@ -20,10 +23,43 @@
 	// Do any additional setup after loading the view, typically from a nib.
 }
 
+- (void)viewDidAppear:(BOOL)animated {
+  [super viewDidAppear:animated];
+
+  self.assetsPickerController = [TSAssetsPickerController new];
+  self.assetsPickerController.delegate = self;
+  self.assetsPickerController.dataSource = self;
+  [self presentViewController:self.assetsPickerController animated:YES completion:nil];
+}
+
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - TSAssetsPickerControllerDataSource
+
+- (void)assetsPickerController:(TSAssetsPickerController *)picker didFinishPickingAssets:(NSArray *)assets {
+
+}
+
+- (void)assetsPickerControllerDidCancel:(TSAssetsPickerController *)picker {
+
+}
+
+- (void)assetsPickerController:(TSAssetsPickerController *)picker failedWithError:(NSError *)error {
+
+}
+
+#pragma mark - TSAssetsPickerControllerDataSource
+
+- (NSUInteger)numberOfItemsToSelectInAssetsPickerController:(TSAssetsPickerController *)picker {
+  return INT_MAX;
+}
+
+- (TSFilter *)filterOfAssetsPickerController:(TSAssetsPickerController *)picker {
+  return [TSFilter filterWithType:FilterTypePhoto];
 }
 
 @end
