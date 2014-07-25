246773bd1db91746131bdb7e02322aab29b4b093
Dennis Schmidt <dmrschmidt@gmail.com>
2014-07-25 20:21:26 +0200

    update icon

---
+++ peekshare/PSSelectViewController.h
@@ -0,0 +1,13 @@
+//
+//  PSSelectViewController.h
+//  peekshare
+//
+//  Created by Dennis Schmidt on 25.07.14.
+//  Copyright (c) 2014 Dennis Schmidt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PSSelectViewController : UIViewController
+
+@end
---
+++ peekshare/PSSelectViewController.m
@@ -0,0 +1,29 @@
+//
+//  PSSelectViewController.m
+//  peekshare
+//
+//  Created by Dennis Schmidt on 25.07.14.
+//  Copyright (c) 2014 Dennis Schmidt. All rights reserved.
+//
+
+#import "PSSelectViewController.h"
+
+@interface PSSelectViewController ()
+
+@end
+
+@implementation PSSelectViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
