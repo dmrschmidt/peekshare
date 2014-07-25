c33cd5ea265918735beb275ee0f49af9c101ba4d
Dennis Schmidt <dmrschmidt@gmail.com>
2014-07-25 21:20:15 +0200

    initial actual but broken viewing

--- peekshare/PSSelectViewController.m
+++ peekshare/PSSelectViewController.m
@@ -8,48 +8,72 @@
 
 #import "PSSelectViewController.h"
 #import "TSAssetsPickerController.h"
+#import "GalleryViewController.h"
+#import "ALAssetAdapter.h"
 
 @interface PSSelectViewController () <UINavigationControllerDelegate, TSAssetsPickerControllerDelegate, TSAssetsPickerControllerDataSource>
-@property(nonatomic, strong) TSAssetsPickerController *assetsPickerController;
+@property(nonatomic) TSAssetsPickerController *assetsPickerController;
+@property(nonatomic) NSMutableArray *adaptedAssets;
+@property(nonatomic) BOOL firstRun;
 @end
 
 @implementation PSSelectViewController
 
 #pragma mark - View Lifecycle
 
+- (id)initWithCoder:(NSCoder *)coder {
+  self = [super initWithCoder:coder];
+  if (self) {
+    _firstRun = YES;
+  }
+
+  return self;
+}
+
+
 - (void)viewDidLoad
 {
-    [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+  [super viewDidLoad];
 }
 
 - (void)viewDidAppear:(BOOL)animated {
   [super viewDidAppear:animated];
 
-  self.assetsPickerController = [TSAssetsPickerController new];
-  self.assetsPickerController.delegate = self;
-  self.assetsPickerController.dataSource = self;
-  [self presentViewController:self.assetsPickerController animated:YES completion:nil];
-}
+  if (self.firstRun) {
+    self.firstRun = NO;
 
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    self.assetsPickerController = [TSAssetsPickerController new];
+    self.assetsPickerController.delegate = self;
+    self.assetsPickerController.dataSource = self;
+    [self presentViewController:self.assetsPickerController animated:YES completion:nil];
+  }
 }
 
 #pragma mark - TSAssetsPickerControllerDataSource
 
 - (void)assetsPickerController:(TSAssetsPickerController *)picker didFinishPickingAssets:(NSArray *)assets {
+  NSLog(@"finished picking %@", assets);
 
+  self.adaptedAssets = [[NSMutableArray alloc] initWithCapacity:assets.count];
+  [assets enumerateObjectsUsingBlock:^(ALAsset *asset, NSUInteger idx, BOOL *stop) {
+      ALAssetAdapter *assetAdapter = [[ALAssetAdapter alloc] init];
+      assetAdapter.asset = asset;
+
+      [self.adaptedAssets addObject:assetAdapter];
+  }];
+
+  GalleryViewController *galleryViewController = [[GalleryViewController alloc] init];
+  galleryViewController.assets = self.adaptedAssets;
+//  [self dismissViewControllerAnimated:NO completion:nil];
+  [self.assetsPickerController presentViewController:galleryViewController animated:NO completion:nil];
 }
 
 - (void)assetsPickerControllerDidCancel:(TSAssetsPickerController *)picker {
-
+  NSLog(@"canceled picking");
 }
 
 - (void)assetsPickerController:(TSAssetsPickerController *)picker failedWithError:(NSError *)error {
-
+  NSLog(@"failed picking");
 }
 
 #pragma mark - TSAssetsPickerControllerDataSource
